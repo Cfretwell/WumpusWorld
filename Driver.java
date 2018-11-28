4570b00fb3eb13b6bc932b5afaf4a60f77153412
Cfretewll <cfretwell97@gmail.com>
2018-11-28 13:26:17 -0700

    WumpusWorld generation working, next need to make agent do things

--- Driver.java
+++ Driver.java
@@ -5,6 +5,11 @@ public class Driver {
 
 
         WumpusWorld four = new WumpusWorld(4);
+        WumpusWorld five = new WumpusWorld(5);
+        WumpusWorld eight= new WumpusWorld(8);
+        WumpusWorld ten  = new WumpusWorld(10);
+
+
 
         System.out.println(" Did a thing");
     }
--- WumpusWorld.java
+++ WumpusWorld.java
@@ -1,36 +1,38 @@
 public class WumpusWorld {
-    boolean [][][] map;  // x, y, c(visited[0], breeze[1], stench[2], glitter[3], pit[4], gold[5], wumpus[6])
+    boolean [][][] map;  // x, y, c(visited[0], pit[1], breeze[2], Gold[3], glitter[4], wumpus[5], stench[6])
 
 
     public WumpusWorld(int dim){
 
         map = new boolean[dim][dim][7];
+        System.out.println("Map dim: "+ dim);
         genMap();
 
-        printMap();
+        //printMap();
     }
 
     private void genMap(){ // generates pits, gold, wumpus, as well as breeze, stench, and glitter
 
-        int G = (int) (Math.random()*((map.length^2)-1)); // generate start spot for gold
-        int W = (int) (Math.random()*((map.length^2)-1)); // generate start spot for wumpus
+        int G = (int) (Math.random()*((map.length*map.length)-1)); // generate start spot for gold
+        int W = (int) (Math.random()*((map.length*map.length)-1)); // generate start spot for wumpus
 
+        System.out.println("Gold at "+ G+ " Wumpus at "+W);
         int count =0;
         for(int x=0;x<map.length;x++){
             for(int y=0;y<map.length;y++){
-                    count++;
+                count++;
                     if(count == G){ // Gold State, no pit
-                        map[x][y][3]= true; // Glitter
-                        map[x][y][5]= true; // Gold
+                        map[x][y][4]= true; // Glitter
+                        map[x][y][3]= true; // Gold
                     }
                     if(count == W){ // Wumpus
-                        map[x][y][6]= true;
-                        setAround(x,y,2); // stench
+                        map[x][y][5]= true;
+                        setAround(x,y,6); // stench
                     }
                     int p = (int) (Math.random() * 5);
                     if(p == 0 && count != G){ // 20% chance pit and no pit on gold
-                        map[x][y][4]= true; // pit
-                        setAround(x,y,1); // breeze
+                        map[x][y][1]= true; // pit
+                        setAround(x,y,2); // breeze
                     }
 
                 }
@@ -40,23 +42,34 @@ public class WumpusWorld {
     }
 
     private void setAround(int x, int y, int sense){ // takes coordinates and sets sense in adjacent cells
+
+        if(sense == 2){
+            System.out.println("pit at "+x+", "+ y);
+        }
+        else if( sense == 6){
+            System.out.println("Wumpus at "+x+", "+ y);
+        }
+        else{
+            System.out.println("Something not good to go setARound");
+
+        }
         if(x-1>=0){
             map[x-1][y][sense]= true;
         }
-        if(x+1<= map.length){
+        if(x+1<= (map.length-1)){
             map[x+1][y][sense]= true;
         }
         if(y-1>=0){
             map[x][y-1][sense]= true;
         }
-        if(y+1<=map.length){
+        if((y+1)<=(map.length-1)){
             map[x][y+1][sense]= true;
         }
     }
 
     public void printMap(){ // prints out the maze or something
 
-        for(int i=0; i<6;i++){
+        for(int i=0; i<7;i++){
             System.out.println(i);
             for(int x=0; x<map.length;x++){
                 for(int y=0;y<map.length;y++){
